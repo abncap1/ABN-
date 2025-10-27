3c5bf2de6be53d80039fbe1d2cac5c02a521ac65
naman.goel <naman.goel@joulewise.com>
2025-10-27 21:03:09 +0530

    fixed why us page

--- components/section/WhyUs.tsx
+++ components/section/WhyUs.tsx
@@ -72,7 +72,7 @@ export function WhyUs() {
 
             {/* Cards */}
             <motion.div
-              className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2 gap-5"
+              className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-5"
               initial="hidden"
               whileInView="visible"
               viewport={{ once: true, amount: 0.2 }}
